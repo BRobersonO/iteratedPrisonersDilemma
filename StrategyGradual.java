0cbe7422c237f641efd9c8232dd842b67c1d79d5
Md Mahfuzur Rahaman <50185394+mahfuz75@users.noreply.github.com>
2021-03-17 02:32:03 -0400

    Add some other strategies

--- StrategyGradual.java
+++ StrategyGradual.java
@@ -3,75 +3,48 @@ import java.util.List;
 
 /**
  * Class containing the gradual strategy.
- * @author	Mahfuz
  */
 public class StrategyGradual extends Strategy
-   {
+{
   /**
-   * Encoding for prober strategy.
-   */
+  * Encoding for gradual strategy.
+  */
 
-  // int iteration;
-  // List<Integer> opponentMoves;
-  int defect_n;// = 0;
-  int punish_count;/// = 0;
-  boolean isHot;// = false;
-  // boolean isTitForTat;
+  int defect_n;
+  int punish_count;
+  boolean isHot;
 
   // 0 = defect, 1 = cooperate
 
-   public StrategyGradual()
-      {
-        name = "Gradual";
-          // iteration = -1;
-          // opponentMoves = new ArrayList<Integer>();
-          // isTitForTat = true;
-      opponentLastMove = 1;
-      isHot = false;
-      punish_count = 0;
-      defect_n = 0;
-    }  /* StrategyProber */
+  public StrategyGradual()
+  {
+    name = "Gradual";
+    opponentLastMove = 1;
+    isHot = false;
+    punish_count = 0;
+    defect_n = 0;
+  }  /* StrategyGradual */
 
-  // public void saveOpponentMove(int move){
-  //   opponentLastMove = move;
-  //   opponentMoves.add(opponentLastMove);
-  //     // System.out.println("Adding " + opponentLastMove);
-  // }
-   public int nextMove()
-      {
-        // iteration++;
-        if(opponentLastMove == 0){
-          // System.out.println("Opp Last: " + opponentLastMove);
-          isHot = true;
-          defect_n++;
-          punish_count = defect_n;
-        }
+  public int nextMove()
+  {
+    if(opponentLastMove == 0){
+      // System.out.println("Opp Last: " + opponentLastMove);
+      isHot = true;
+      defect_n++;
+      punish_count = defect_n;
+    }
 
-        if(isHot == true){
-          punish_count--;
-          if(punish_count == -2){
-            isHot = false;
-          }
-          if(punish_count < 0){
-            return 1;
-          }
-          return 0;
-        }
-        return 1;
-
-
-        // if(iteration < probe.length()) {
-        //     return Character.getNumericValue(probe.charAt(iteration));
-        // }
-        // else if(isTitForTat) {
-        //     //always cooperate when playing agaisnt TitForTat
-        //     // return opponentLastMove;
-        //     return 1;
-        // }
-        // else {
-        //     //Always Defect
-        //     return 0;
-        // }
+    if(isHot == true){
+      punish_count--;
+      if(punish_count == -2){
+        isHot = false;
       }
+      if(punish_count < 0){
+        return 1;
+      }
+      return 0;
+    }
+    return 1;
+  }
 
-   }  /* class StrategyTitForTat */
+}  /* class StrategyGradual */
